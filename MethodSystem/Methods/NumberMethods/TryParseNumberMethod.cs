ff195fbc5536c195ac061f266c089c0789926385
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-10 16:41:27 +0100

    Create TryParseNumberMethod.cs

---
+++ MethodSystem/Methods/NumberMethods/TryParseNumberMethod.cs
@@ -0,0 +1,36 @@
+﻿using SER.ArgumentSystem.Arguments;
+using SER.ArgumentSystem.BaseArguments;
+using SER.MethodSystem.BaseMethods;
+using SER.MethodSystem.Structures;
+using SER.TokenSystem.Tokens;
+using SER.ValueSystem;
+
+namespace SER.MethodSystem.Methods.NumberMethods;
+
+public class TryParseNumberMethod : ReferenceReturningMethod<ParseResult<NumberValue>>
+{
+    public override string Description => "Tries to parse a given value to a number.";
+
+    public override Argument[] ExpectedArguments =>
+    [
+        new AnyValueArgument("value to parse")
+    ];
+
+    public override void Execute()
+    {
+        var valueToParse = Args.GetAnyValue("value to parse");
+        if (valueToParse is NumberValue numVal)
+        {
+            ReturnValue = new(numVal.ExactValue);
+            return;
+        }
+
+        if (BaseToken.TryParse<NumberToken>(valueToParse.ToString(), Script).WasSuccessful(out var token))
+        {
+            ReturnValue = new(token.Value);
+            return;
+        }
+        
+        ReturnValue = new(null);
+    }
+}
