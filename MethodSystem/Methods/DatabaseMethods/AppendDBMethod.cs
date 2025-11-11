2ace6ff848a0ee8cedade515d9670236b7c195dc
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-11 11:33:24 +0100

    Create AppendDBMethod.cs

---
+++ MethodSystem/Methods/DatabaseMethods/AppendDBMethod.cs
@@ -0,0 +1,33 @@
+﻿using SER.ArgumentSystem.Arguments;
+using SER.ArgumentSystem.BaseArguments;
+using SER.Helpers.Exceptions;
+using SER.MethodSystem.BaseMethods;
+using SER.MethodSystem.MethodDescriptors;
+
+namespace SER.MethodSystem.Methods.DatabaseMethods;
+
+public class AppendDBMethod : SynchronousMethod, ICanError
+{
+    public override string Description => "Adds a key-value pair to the database.";
+
+    public string[] ErrorReasons =>
+    [
+        "Provided value cannot be stored in databases"
+    ];
+
+    public override Argument[] ExpectedArguments =>
+    [
+        new DatabaseArgument("database"),
+        new TextArgument("key"),
+        new AnyValueArgument("value")
+    ];
+
+    public override void Execute()
+    {
+        if (Args.GetDatabase("database").Set(
+            Args.GetText("key"), 
+            Args.GetAnyValue("value")
+        ).HasErrored(out var error)) 
+            throw new ScriptRuntimeError(error);
+    }
+}
