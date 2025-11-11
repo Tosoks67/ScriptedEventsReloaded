b778fcad208d631976490fc7f28a125789736302
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-11 11:33:56 +0100

    Create Database.cs

---
+++ FileSystem/Structures/Database.cs
@@ -0,0 +1,127 @@
+﻿using LabApi.Features.Wrappers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SER.ArgumentSystem.Arguments;
+using SER.Helpers.Extensions;
+using SER.Helpers.ResultSystem;
+using SER.ValueSystem;
+using Db = System.Collections.Generic.Dictionary<string, SER.FileSystem.Structures.Database.DatabaseValue>;
+
+namespace SER.FileSystem.Structures;
+
+public class Database
+{
+    public readonly struct DatabaseValue(Type originalType, object value)
+    {
+        public string Type { get; } = originalType.GetAccurateName();
+        public object Value { get; } = value;
+    }
+    
+    private static readonly JsonSerializerSettings Settings = new()
+    {
+        TypeNameHandling = TypeNameHandling.Arrays
+    };
+    
+    private readonly string _path;
+    private readonly string _name;
+    private readonly Db _db;
+
+    protected Database(string path, Db db)
+    {
+        _path = path;
+        _db = db;
+        _name = Path.GetFileNameWithoutExtension(path);
+        AllDatabases.Add(this);
+    }
+
+    private static readonly List<Database> AllDatabases = [];
+
+    public static void Create(string name)
+    {
+        Directory.CreateDirectory(FileSystem.DbDirPath);
+        var path = Path.Combine(FileSystem.DbDirPath, $"{name}.json");
+        if (File.Exists(path)) return;
+
+        using var file = File.CreateText(path);
+        file.Write("{}");
+        file.Close();
+    }
+    
+    public static TryGet<Database> TryGet(string name)
+    {
+        if (AllDatabases.FirstOrDefault(d => d._name == name) is { } foundDb)
+        {
+            return foundDb;
+        }
+        
+        var path = Path.Combine(FileSystem.DbDirPath, $"{name}.json");
+        if (!File.Exists(path))
+        {
+            return $"There is no database called '{name}'";
+        }
+        
+        string content = File.ReadAllText(path);
+        if (JsonConvert.DeserializeObject<Db>(content, Settings) is not { } db)
+        {
+            return $"Database '{name}' is corrupted!";
+        }
+        
+        return new Database(path, db);
+    }
+
+    public Result Set(string key, Value value, bool save = true)
+    {
+        object saveVal;
+        switch (value)
+        {
+            case LiteralValue literalValue:
+                saveVal = literalValue.Value;
+                break;
+            case PlayerValue playerValue:
+                saveVal = playerValue.Players.Select(p => p.UserId).ToArray();
+                break;
+            default:
+                return $"Value '{value}' cannot be stored in databases";
+        }
+        
+        _db[key] = new(value.GetType(), saveVal);
+        if (save) Save();
+        return true;
+    }
+
+    public TryGet<Value> Get(string key)
+    {
+        if (!_db.TryGetValue(key, out var val))
+        {
+            return $"There is no key called '{key}' in the '{_name}' database.";
+        }
+
+        if (val.Value is null)
+        {
+            return $"Value of key '{key}' is not set.";
+        }
+
+        if (val.Type == typeof(PlayerValue).GetAccurateName())
+        {
+            if (val.Value is not string[] playerIds)
+            {
+                return $"Value for key '{key}' is corrupted";
+            }
+
+            return Value.Parse(Player.List.Where(p => playerIds.Contains(p.UserId)));
+        }
+
+        if (Value.Parse(val.Value) is {} value && value.GetType().GetAccurateName() == val.Type)
+        {
+            return value;
+        }
+
+        return $"Value for key '{key}' is corrupted";
+    }
+
+    public void Save()
+    {
+        string json = JsonConvert.SerializeObject(_db, Formatting.Indented, Settings);
+        File.WriteAllText(_path, json);
+    }
+}
